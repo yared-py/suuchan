6733940b1564826588c4aadb0dbe3054e3515ec5
Yared <elizondoyared@gmail.com>
2019-12-16 19:48:09 -0600

    Se agreagaron las clases Talk, Time, Query para hacer el saludo de suuchan

--- src/Modelo/Index.py
+++ src/Modelo/Index.py
@@ -23,55 +23,43 @@ class Talk:
         print('Cleo: ' + audio)
         engine.say(audio)
         engine.runAndWait()
-        
+
 voz = Talk()
-def greetMe():
-    currentH = int(datetime.datetime.now().hour)
-    if currentH >= 0 and currentH < 12:
-        voz.speak('good morning sir Elizondo!')
 
-    if currentH >= 12 and currentH < 18:
-        voz.speak('Good afternoon sir Elizondo!')
+class time:
+    @staticmethod
+    def greetMe():
+        currentH = int(datetime.datetime.now().hour)
+        if currentH >= 0 and currentH < 12:
+            voz.speak('good morning!')
 
-    if currentH >= 18 and currentH !=0:
-        voz.speak('Good night sir Elizondo!')
+        if currentH >= 12 and currentH < 18:
+            voz.speak('Good afternoon!')
 
-greetMe()
+        if currentH >= 18 and currentH !=0:
+            voz.speak('Good night!')
 
-def bye():
-    currentH = int(datetime.datetime.now().hour)
-    #print(currentH)
-    if currentH >= 0 and currentH < 12:
-        voz.speak('good bye sir Elizondo have a nice day.')
-        voz.speak('Its ' + currentH)
-        sys.exit()
-
-    if currentH >= 12 and currentH < 18:
-        voz.speak('good bye sir Elizondo have a good afternoon.')
-        #speak('Its ' + str(currentH) + 'Oclock')
-        sys.exit()
-
-    if currentH >= 18 and currentH !=0:
-        voz.speak('good bye sir Elizondo have a good nigth.')
-        sys.exit()
-
-
-voz.speak('hello sir Elizondo, Im cleo your artificial intelligence')
-voz.speak('How can I help you?')
-
-def myCommand():
+class Query:
+    def myCommand():
    
-    r = sr.Recognizer()                                                                                   
-    with sr.Microphone() as source:                                                                       
-        print("Escuchando...")
-        r.pause_threshold =  1
-        audio = r.listen(source)
-    try:
-        query = r.recognize_google(audio, language='en-in')
-        print('User: ' + query + '\n')
+        r = sr.Recognizer()                                                                                   
+        with sr.Microphone() as source:                                                                       
+            print("Escuchando...")
+            r.pause_threshold =  1
+            audio = r.listen(source)
+        try:
+            query = r.recognize_google(audio, language='en-in')
+            print('User: ' + query + '\n')
         
-    except sr.UnknownValueError:
-        voz.speak('Im sorry Sir Elizondo tries typing the command!')
-        query = str(input('Command: '))
+        except sr.UnknownValueError:
+            voz.speak('Im sorry Sir Elizondo tries typing the command!')
+            query = str(input('Command: '))
 
-    return query
+        return query
+
+
+time = time()
+
+time.greetMe()
+voz.speak('onii chan, Im suuchan')
+voz.speak('How can I help you?')
